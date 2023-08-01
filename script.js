157eb2134306d49dbbb3d9579fc25561186c041a
ismail306 <ismail.diuswe@gmail.com>
2023-08-01 14:21:42 +0600

    deposit withdraw done

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 document.getElementById("login").addEventListener("click", function () {
   var user = document.getElementById("username").value;
   var pass = document.getElementById("password").value;
-  if (user == "Admin" && pass == "pass") {
+  if (user == "" && pass == "") {
     document.getElementById("login-area").style.display = "none";
 
     document.getElementById("transection-area").style.display = "block";
-    alert("Login successful");
   } else {
     alert("Login failed");
   }
+});
+
+document.getElementById("depositBtn").addEventListener("click", function () {
+  const depositAmount = parseFloat(
+    document.getElementById("depositAmount").value
+  );
+  const deposit = parseFloat(document.getElementById("deposit").innerText);
+  const totalDeposit = depositAmount + deposit;
+  document.getElementById("deposit").innerText = totalDeposit;
+  document.getElementById("balance").innerHTML = totalDeposit;
+
+  document.getElementById("depositAmount").value = "";
+});
+
+document.getElementById("withdrawBtn").addEventListener("click", function () {
+  const withdrawAmount = parseFloat(
+    document.getElementById("withdrawAmount").value
+  );
+  const withdraw = parseFloat(document.getElementById("withdraw").innerText);
+  const totalWithdraw = withdrawAmount + withdraw;
+  const balance = parseFloat(document.getElementById("balance").innerText);
+  document.getElementById("balance").innerText = balance - totalWithdraw;
+  document.getElementById("withdraw").innerText = totalWithdraw;
+  document.getElementById("withdrawAmount").value = "";
 });
